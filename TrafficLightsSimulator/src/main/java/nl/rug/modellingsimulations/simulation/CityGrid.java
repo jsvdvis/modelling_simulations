ffdec25fbf70b91b7cfaf24523e54fb5aab20700
jaap <jaap@vdvis.com>
2020-09-29 08:49:48 +0200

    Remove comments

--- TrafficLightsSimulator/src/main/java/nl/rug/modellingsimulations/simulation/CityGrid.java
+++ TrafficLightsSimulator/src/main/java/nl/rug/modellingsimulations/simulation/CityGrid.java
@@ -69,44 +69,13 @@ public class CityGrid implements Simulation {
             VehicleSourceNavigableNode leftColumnSource = new VehicleSourceNavigableNode(new Point(-4, i * 4 + 1));
             builder.connect(leftColumnJunction, leftColumnSink);
             builder.connect(leftColumnSource, leftColumnJunction);
-//
+
             TrafficLightJunction rightColumnJunction = junctionGrid.get(i).get(N - 1);
             VehicleSinkNavigableNode rightColumnSink = new VehicleSinkNavigableNode(new Point(N * 4, i * 4 - 1));
             VehicleSourceNavigableNode rightColumnSource = new VehicleSourceNavigableNode(new Point(N * 4, i * 4 + 1));
             builder.connect(rightColumnJunction, rightColumnSink);
             builder.connect(rightColumnSource, rightColumnJunction);
         }
-
-//        VehicleSourceNavigableNode vehicleSourceNavigableNode = new VehicleSourceNavigableNode(new Point(0, 5));
-//        VehicleSinkNavigableNode vehicleSinkNavigableNode1 = new VehicleSinkNavigableNode(new Point(0,-1));
-//        VehicleSinkNavigableNode vehicleSinkNavigableNode2 = new VehicleSinkNavigableNode(new Point(4,5));
-//
-//        TrafficLightJunction junction1 = new SimpleTrafficLightJunction(new Point(0, 0));
-//        junction1.setTrafficLightStrategy(new RoundRobinTimerTrafficLightStrategy(junction1));
-//        TrafficLightJunction junction2 = new SimpleTrafficLightJunction(new Point(0, 4));
-//        junction2.setTrafficLightStrategy(new RoundRobinTimerTrafficLightStrategy(junction2));
-//        TrafficLightJunction junction3 = new SimpleTrafficLightJunction(new Point(4, 4));
-//        junction3.setTrafficLightStrategy(new RoundRobinTimerTrafficLightStrategy(junction3));
-//        TrafficLightJunction junction4 = new SimpleTrafficLightJunction(new Point(4, 0));
-//        junction4.setTrafficLightStrategy(new RoundRobinTimerTrafficLightStrategy(junction4));
-
-//        builder.connectTwoWayJunction(junction1, junction2);
-//        builder.connect(junction2, junction3);
-//        builder.connect(junction3, junction1);
-//        builder.connect(junction1, junction3);
-
-//        builder.connect(vehicleSourceNavigableNode, junction2);
-//        builder.connect(junction1, vehicleSinkNavigableNode1);
-//        builder.connect(junction3, vehicleSinkNavigableNode2);
-
-//        builder.connectTwoWayJunction(junction1, junction2);
-//        builder.connectTwoWayJunction(junction1, junction4);
-//        builder.connectTwoWayJunction(junction2, junction3);
-//        builder.connectTwoWayJunction(junction3, junction4);
-//
-//        builder.connect(vehicleSourceNavigableNode, junction2);
-//        builder.connect(junction1, vehicleSinkNavigableNode1);
-//        builder.connect(junction3, vehicleSinkNavigableNode2);
 
         builder.build();
         builder.buildExemptedLanes();
