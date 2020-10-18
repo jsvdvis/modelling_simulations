ab29d0705195773bd0f9bf2b83e82b51a3ab4f5b
Mark Soelman <info@marksoelman.nl>
2020-10-18 11:53:26 +0200

    Save max vehicle waiting time

--- TrafficLightsSimulator/src/main/java/nl/rug/modellingsimulations/metrics/AveragedVehicleWaitingTime.java
+++ TrafficLightsSimulator/src/main/java/nl/rug/modellingsimulations/metrics/AveragedVehicleWaitingTime.java
@@ -6,12 +6,14 @@ public class AveragedVehicleWaitingTime extends SimulationMetricsMeasurer {
     private final int STEPS_PER_AVERAGE = 100;
     private int stepCounter = 0;
     private long vehicleWaitingTime = 0;
+    private long maxWaitingTimeForStep = 0;
 
     public AveragedVehicleWaitingTime(MetricsStepResultSaver saver) {
         super(saver);
         this.addDataToSaver("Vehicle waiting time");
         this.addDataToSaver("Total Number of Vehicles");
         this.addDataToSaver("Relative waiting time vehicles");
+        this.addDataToSaver("Maximum Vehicle Waiting Time");
         super.finishSimulationStep(null);
     }
 
@@ -30,14 +32,18 @@ public class AveragedVehicleWaitingTime extends SimulationMetricsMeasurer {
             this.addDataToSaver(String.valueOf(averageWaitingTime));
             this.addDataToSaver(String.valueOf(numberOfVehicles));
             this.addDataToSaver(String.valueOf(averageWaitingTime / numberOfVehicles));
+            this.addDataToSaver(String.valueOf(maxWaitingTimeForStep));
             super.finishSimulationStep(simulation);
 
             stepCounter = 0;
             vehicleWaitingTime = 0;
+            maxWaitingTimeForStep = 0;
         }
     }
 
     public void addVehicleWaitingTime(int waitingTime) {
         this.vehicleWaitingTime += waitingTime;
+        if(waitingTime > maxWaitingTimeForStep)
+            maxWaitingTimeForStep = waitingTime;
     }
 }
